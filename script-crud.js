b0837a46c9e2be23a8af7e88fd0a0cc43ae93f6c
Jhoni Conzatti <jhoni.conzatti@gmail.com>
2024-09-16 14:04:37 -0300

    Update script-crud.js

--- script-crud.js
+++ script-crud.js
@@ -1,1 +1,18 @@
-const ElementoBotaoAdicionarTarefa = document.getElementById("start-pause");
+const ElementoBotaoAdicionarTarefa = document.querySelector(".app__button--add-task");
+const ElementoFormularioAdicionarTarefa = document.querySelector(".app__form-add-task");
+
+const Tarefas = [];
+
+ElementoBotaoAdicionarTarefa.addEventListener("click", () => {
+    ElementoFormularioAdicionarTarefa.classList.toggle("hidden");
+});
+
+ElementoFormularioAdicionarTarefa.addEventListener("submit", (pEvento) => {
+    pEvento.preventDefault();
+    const lElementoCampoTarefa = document.querySelector(".app__form-textarea");
+    const lTarefa = {
+        descricao: lElementoCampoTarefa.value
+    }
+    Tarefas.push(lTarefa);
+    localStorage.setItem("tarefas", Tarefas);
+});
